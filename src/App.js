c847db8ba3aa8c5f39d19cd68609aef781b119bb
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-08 19:14:03 +0300

    SetState

--- src/App.js
+++ src/App.js
@@ -7,8 +7,15 @@ function App () {
   const [isRed , setRed] = useState(false);
   const [count, setCount] = useState(0);
 
+
+  const increment = () =>{
+    setCount(count +1);
+  }
+
   return(
     <div className="app">
+      <button onClick = {increment}>Increment</button>
+      <h1>{count}</h1>
       
     </div>
   );
