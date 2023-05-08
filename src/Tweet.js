a4cbd3254b560ac97eea785978598bf90d47d587
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-05-08 16:08:51 +0300

    Tweet Component

---
+++ src/Tweet.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+
+function Tweet (){
+    return(
+        <div className="tweet">
+            <h1>Name</h1>
+            <p>This is my first tweet</p>
+            <h4>Number of Likes</h4>
+        </div>
+    );
+}
+
+export default Tweet;
